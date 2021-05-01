5ae01e3e0d8acb38b34aabc08630413fe8ad4d3f
NawafSwe <nawaf.jr9@hotmail.com>
2021-05-01 23:47:01 +0300

    feature: server running fine

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('back to work');
+// importing packages
+
+import {Response, Request, NextFunction, Router, Express, Application} from 'express';
+
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
+const app: Application = express();
+
+// config express app
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+    res.json("works").status(200);
+});
+
+// config the port and host
+dotenv.config();
+const PORT: number = Number(process.env.PORT) || 3000;
+const HOST: string = process.env.HOST || "localhost";
+
+app.listen(PORT, HOST, async () => {
+    console.log(`server running http://${HOST}:${PORT}`);
+});
