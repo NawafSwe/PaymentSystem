1f59b23eab726520096a24983e47eb3c52122eff
NawafSwe <nawaf.jr9@hotmail.com>
2021-05-02 04:00:28 +0300

    feature: making payments populate user data

--- src/controllers/paymentsController.ts
+++ src/controllers/paymentsController.ts
@@ -5,7 +5,9 @@ import {paymentModel as Payment} from '../models/payment/paymentModel';
 
 export async function getPayments(req: Request, res: Response, next?: NextFunction) {
     try {
-        res.json(await Payment.find({}));
+        // finding all payments with user
+        const payments = await Payment.find({}).populate('_customerId').exec();
+        res.json(payments).status(200);
     } catch (error: any) {
         res.json({message: `${error.message}`, code: 400, status: 'Bad Request'}).status(400);
     }
@@ -26,13 +28,11 @@ export async function getPaymentById() {
 // used to patch payment to change it partially and make its status deleted
 export async function patchPayment(req: Request, res: Response, next?: NextFunction) {
     try {
-        const payment: IPayment | never | null = await Payment.findById(req.params.id);
+        const payment: IPayment | never | null = await Payment.findById(req.params.id).populate('_customerId').exec();
         if (payment) {
             payment.isDeleted = true;
             await payment.save();
             res.json(payment).status(203);
-        } else {
-            res.json({message: `payment with ${req.params.id} is not exists`, code: 404, status: 'Not Found'});
         }
 
     } catch (error: any) {
--- src/models/payment/paymentModel.ts
+++ src/models/payment/paymentModel.ts
@@ -1,14 +1,16 @@
 import {IPayment} from "./IPayment";
 import * as mongoose from 'mongoose';
-import {Schema, Model, model} from "mongoose";
+import {Schema, Model, model, Mongoose, Types} from "mongoose";
+
 
 const schema: Schema = new mongoose.Schema({
-    _customerId: {required: true, type: mongoose.Types.ObjectId, ref: 'User'},
+    _customerId: {required: true, type: Schema.Types.ObjectId, ref: 'user'},
     amount: {required: true, type: Number},
     currency: {required: true, type: String},
     dueDate: {type: Date, default: Date() + 30 * 24 * 60 * 60 * 1000},
-    paidDate: {type: Date, required: true},
-    isDeleted: {type: Boolean, default: false}
+    paidDate: {type: Date, required: false},
+    isDeleted: {type: Boolean, default: false},
+    status: {type: String, default: 'NC'}
 
 
 });
