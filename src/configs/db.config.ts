f86a0ccde05546e050c5b0bf145d02e2be0d5c1e
NawafSwe <nawaf.jr9@hotmail.com>
2021-05-03 01:52:09 +0300

    document: document database files

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -1,5 +1,21 @@
+/**
+ * @module src/configs/db.config.ts
+ * @requires Mongoose
+ * @description this module is related to database configuration and connection
+ */
+
 import Mongoose from 'mongoose';
 
+/**
+ * @async
+ * @function
+ * @namespace connect
+ * @param uri uri of the database
+ * @description establish connection to database
+ * @return Promise<void|never> void function
+ * @throws error if there is any problem occurred
+ *
+ */
 export async function connect(uri: string): Promise<void | never> {
     try {
         await Mongoose.connect(uri, {
