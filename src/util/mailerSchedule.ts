7bc305baa862309324a3cae760c36d80d902cadc
NawafSwe <nawaf.jr9@hotmail.com>
2021-05-03 00:34:25 +0300

    mailSchedule work 100% as excpected

--- src/util/mailerSchedule.ts
+++ src/util/mailerSchedule.ts
@@ -1,31 +1,22 @@
 // requiring packages
-
 import nodemailer from 'nodemailer';
 
 const nodeScheduler = require('node-schedule');
-// mail options
-let mailOptions = {
-    from: 'Test',
-    to: '',
-    subject: 'Payment information',
-    html: `<b>Dear Payment is overdue</b>`
-};
 
-
-// transporter to transport the email
-let transporter = nodemailer.createTransport({
-    service: process.env.API_EMAIL_PROVIDER,
-    secure: true,
-    host: 'smtp.gmail.com',
-    port: 465,
-    auth: {
-        user: process.env.API_GUARD,
-        pass: process.env.API_GUARD_PASS
-    }
-});
 
 async function sendEmail(mailOptions: any) {
     try {
+        // transporter to transport the email
+        let transporter = nodemailer.createTransport({
+            service: process.env.API_EMAIL_PROVIDER,
+            secure: true,
+            host: 'smtp.gmail.com',
+            port: 465,
+            auth: {
+                user: process.env.API_GUARD,
+                pass: process.env.API_GUARD_PASS
+            }
+        });
         await transporter.sendMail(mailOptions);
         console.log(`sent`);
     } catch (error: any) {
@@ -37,18 +28,36 @@ async function sendEmail(mailOptions: any) {
 export async function scheduleEmail(userData: any, date: Date) {
     try {
 
+        // we must using date , but for testing we want to send email at any given time xx:x 1 in second 1.
         let job = nodeScheduler.scheduleJob('* * * * 1', () => {
-            mailOptions.to = userData.email;
-            sendEmail(mailOptions);
+            sendEmail(prepareEmail(userData.email));
             // after sending an email invalidate this job
             job.cancel();
             console.log(`job canceled for user with id ${userData._id}`);
+            // after sending an email we wants to mark payment as completed and update paidDate and status
+            const {payment} = userData;
+            // means completed
+            payment.status = 'CP';
+            payment.isDeleted = true;
+            payment.paidDate = new Date();
+            // saving changes
+            payment.save();
         });
 
 
     } catch (error: any) {
         console.log(`error happened in scheduleEmail error: ${error.message} `);
     }
+}
+
+function prepareEmail(deliverTo: string): any {
+    // mail options
+    return {
+        from: 'Test',
+        to: deliverTo,
+        subject: 'Payment information',
+        html: `<b>Dear Payment is overdue</b>`
+    };
 }
 
 
