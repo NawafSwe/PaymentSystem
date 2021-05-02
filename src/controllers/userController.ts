d4d8c6a0ccf0afe5192368a9ab28fc918ad4b621
NawafSwe <nawaf.jr9@hotmail.com>
2021-05-03 02:16:01 +0300

    doc: documenting user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,9 +1,27 @@
-// use next if there is no res.json();
+/**
+ * @module src/controllers/userController.ts
+ * @description module orchestrate the user collection with the database and send responses back to client
+ * @requires{Response,Request,NextFunction}
+ * @requires IUser
+ * @requires User
+ *
+ *
+ */
 import {Response, Request, NextFunction} from "express";
 import {IUser} from "../models/user/IUser";
 import {userModel as User} from '../models/user/userModel';
 
 
+/**
+ * @async
+ * @function
+ * @namespace getUsers
+ * @param req express request
+ * @param res express response
+ * @param next next func
+ * @description getting all users from database
+ * @returns Promise<void> nothing returned it is only sending response back to the client
+ */
 export async function getUsers(req: Request, res: Response, next?: NextFunction) {
     try {
         res.json(await User.find({}));
@@ -13,6 +31,16 @@ export async function getUsers(req: Request, res: Response, next?: NextFunction)
     }
 }
 
+/**
+ * @async
+ * @function
+ * @namespace createUser
+ * @param req express request
+ * @param res express response
+ * @param next next func
+ * @description creating new user to the database
+ * @returns Promise<void> nothing returned it is only sending response back to the client
+ */
 export async function createUser(req: Request, res: Response, next?: NextFunction) {
     try {
         res.json(await User.create<IUser>(req.body)).status(201);
@@ -22,6 +50,16 @@ export async function createUser(req: Request, res: Response, next?: NextFunctio
     }
 }
 
+/**
+ * @async
+ * @function
+ * @namespace getUserById
+ * @param req express request
+ * @param res express response
+ * @param next next func
+ * @description getting user by id from database
+ * @returns Promise<void> nothing returned it is only sending response back to the client
+ */
 export async function getUserById(req: Request, res: Response, next?: NextFunction) {
     try {
         res.json(await User.findById(req.params.id));
