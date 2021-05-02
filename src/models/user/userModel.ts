ef0c29bf1f7a5c4c6b8d4f40374e56830cc49c7e
NawafSwe <nawaf.jr9@hotmail.com>
2021-05-03 00:35:14 +0300

    making userModel with email to be unique

--- src/models/user/userModel.ts
+++ src/models/user/userModel.ts
@@ -3,7 +3,7 @@ import {IUser} from "./IUser";
 
 const schema: Schema = new Schema({
     name: {required: true, type: String},
-    email: {required: true, type: String},
+    email: {required: true, type: String, unique: true},
     isDeleted: {type: Boolean, default: false},
     payments: [{type: Schema.Types.ObjectId, ref: 'payment'}],
 });
